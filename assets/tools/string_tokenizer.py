91ac0bfb396ad4d171977af82f9a400411e01827
Christian Gehlen <chr.schmitz01@gmail.com>
2024-01-13 12:27:14 +0100

    Added distributions for american species

--- assets/tools/string_tokenizer.py
+++ assets/tools/string_tokenizer.py
@@ -1,4 +1,4 @@
-input = "Saudi-Arabien, Vereinigte Arabische Emirate (VAE), Oman, Jemen (einschließlich Socotra und Abd al Kuri), Iran, Indien"
+input = "USA (spezifisch: Ein Bereich, der sich von Südost-North Carolina bis Süd-Nevada erstreckt), Mexiko, Länder in Zentralamerika, die sich bis Zentral-Panama erstrecken (dazu gehören beispielsweise Guatemala, Honduras, El Salvador, Nicaragua und Costa Rica), Panama (spezifisch: Ehemalige Kanalzone und die Perleninseln)"
 
 for i in input.split(','):
     print(f'- {i.strip()}')
